908f42d4497ab7ef871a69477b3290213e94300e
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-14 17:49:09 +0100

    operadores de comparación

--- JS/operadores.js
+++ JS/operadores.js
@@ -6,6 +6,14 @@ document.getElementById("result2").innerHTML = "7 - 4 = " + (x-y);
 document.getElementById("result3").innerHTML = "7 * 4 = " + (x*y);
 document.getElementById("result4").innerHTML = "7 / 4 = "+ (x/y);
 document.getElementById("result5").innerHTML = "7 % 4 = "+ (x%y);
-document.write("<p>El valor de x es: " + x + "</p>");
-document.write("<p>El valor de x es: " + x++ + "</p>");
-document.write("<p>El valor de x es: " + ++x + "</p>");
+// x=x+1 <==> x++ <==> ++x
+console.log("El valor de x es: " + x);   //x es 7
+//x se muestra el 7 x es 8
+console.log("El valor de x++ es: " + x++); 
+//Mostramos el valor x
+console.log("El valor de x es: " + x);
+console.log("El valor de ++x es: " + ++x); //x es 9
+//Mostramos el valor x
+console.log("El valor de x es: " + x);
+//COMPARACIÓN
+document.getElementById("resultadocomparadores").innerHTML = "Resultado de x!=y? " + (x!=y);
