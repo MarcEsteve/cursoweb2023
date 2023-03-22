05c511644654b025b4cc55ff07371aeec5e94556
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-22 18:12:28 +0100

    ejercicios 21 a 25

---
+++ JS/ejercicio21-25.js
@@ -0,0 +1,15 @@
+//Declarar
+var x,y,z,w,k;
+//Asignar 
+x= 10;
+y=13;
+z=3;
+w= 100;
+k=10;
+//Cálculos
+x += 5;
+y-=7;
+z*=10;
+w/=10;
+k%=3;
+document.getElementById("ejer2125").innerHTML = "Muestra la variable X: " + x + " muestra Y: " + y + " muestra Z: " + z + " muestra W: " + w + " muestra K: " + k;
