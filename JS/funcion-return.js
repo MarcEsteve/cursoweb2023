9c29d30605c27638122799c4ed086fc8d0a9b48a
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-27 17:35:40 +0200

    funciones flecha

--- JS/funcion-return.js
+++ JS/funcion-return.js
@@ -18,15 +18,17 @@ document.getElementById("res").innerHTML =
   "El resultado de restar 13 - 3 es " + w;
 
 //Funciones
-function multiplicar(a, b) {
-  return a * b;
-}
-function dividir(a, b) {
-  return a / b;
-}
-function sumar(a, b) {
-  return a + b;
-}
-function restar(a, b) {
-  return a - b;
-}
+// function multiplicar(a, b) {
+//   return a * b;
+// }
+// function dividir(a, b) {
+//   return a / b;
+// }
+// function sumar(a, b) {
+//   return a + b;
+// }
+// function restar(a, b) {
+//   return a - b;
+// }
+
+//Funciones de flecha
