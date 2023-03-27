5da1678690c385195152ee4405c2674959211b27
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-27 16:58:13 +0200

    más funciones

--- JS/funcion-return.js
+++ JS/funcion-return.js
@@ -1,20 +1,32 @@
 //Declarar
-var x, y;
+var x, y, z, w;
 
 //Asignar
-x = multiplica(4, 3);
-y = divide(8,4);
+x = multiplicar(4, 3);
+y = dividir(8, 4);
+z = sumar(3, 4);
+w = restar(13, 3);
 
 //Acciones
-document.getElementById("mult").innerHTML = "El resultado de multiplicar 4 * 3 es " + x;
-document.getElementById("div").innerHTML = "El resultado de dividir 8 / 4 es " + y;
-
+document.getElementById("mult").innerHTML =
+  "El resultado de multiplicar 4 * 3 es " + x;
+document.getElementById("div").innerHTML =
+  "El resultado de dividir 8 / 4 es " + y;
+document.getElementById("sum").innerHTML =
+  "El resultado de sumar 3 + 4 es " + z;
+document.getElementById("res").innerHTML =
+  "El resultado de restar 13 - 3 es " + w;
 
 //Funciones
-function multiplica(a, b) {
+function multiplicar(a, b) {
   return a * b;
 }
-
-function divide(a, b) {
-    return a / b;
-  }
+function dividir(a, b) {
+  return a / b;
+}
+function sumar(a, b) {
+  return a + b;
+}
+function restar(a, b) {
+  return a - b;
+}
