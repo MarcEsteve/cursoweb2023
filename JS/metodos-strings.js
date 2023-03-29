3e92b2c5ffeae0f2f7073ad9e22760b63b1b5c61
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-29 18:37:38 +0200

    ejercicios de metodos javascript

--- JS/metodos-strings.js
+++ JS/metodos-strings.js
@@ -18,7 +18,51 @@ console.log(str);
 
 //Método replace()
 
-function reemplaza() {
+function reemplazar() {
   let textoReemplazable = document.getElementById("replace").innerHTML;
-  document.getElementById("replace").innerHTML = textoReemplazable.replace("Microsoft", "W3Schools");
+  document.getElementById("replace").innerHTML = textoReemplazable.replace(
+    "Microsoft",
+    "W3Schools"
+  );
+  //   replace(/MICROSOFT/i, "W3Schools"); //reemplaza con cualquier mayúscula y minúscula de la palabra /i uso del flag
 }
+//replaceAll()
+let textoGatosPerros =
+  "I love cats. Cats are very easy to love. Cats are very popular.";
+console.log(textoGatosPerros);
+textoGatosPerros = textoGatosPerros.replaceAll("Cats", "Dogs");
+console.log(textoGatosPerros);
+textoGatosPerros = textoGatosPerros.replaceAll("cats", "dogs");
+console.log(textoGatosPerros);
+
+document.getElementById("todo").innerHTML = textoGatosPerros;
+
+//Concat()
+let nombre = "Marc";
+let apellido = "Esteve Garcia";
+let nombreCompleto = nombre.concat(" ", apellido);
+console.log(nombreCompleto);
+
+//trim()
+
+let text1 = "     Hello World!     ";
+let text2 = text1.trim();
+
+console.log(text1);
+console.log(text2);
+
+document.getElementById("trim").innerHTML =
+  "Longitud text1 = " + text1.length + "<br>Longitud text2 = " + text2.length;
+
+//padStart()
+
+let textoPadeado = "50";
+textoPadeado = textoPadeado.padStart(8,"x");
+console.log(textoPadeado);
+
+document.getElementById("padStart").innerHTML = textoPadeado;
+//padEnd() equivalente a padStart() pero al final del texto
+
+//charAt()
+var textoBuscar = "HELLO WORLD";
+console.log(textoBuscar.charAt(6));
