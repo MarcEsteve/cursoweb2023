ab2ec322b46c75d7537fb8d1076820e1d84ffb42
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-29 16:39:52 +0200

    replace

--- JS/metodos-strings.js
+++ JS/metodos-strings.js
@@ -11,7 +11,14 @@ part = texto.slice(-12); //Banana, Kiwi (los 12 últimos valores)
 console.log(part);
 part = texto.slice(-12, -6); //Banana (del -12 al -6 no incluido)
 console.log(part);
-
+//Substring método
 let str = "Manzana, Plátano, Kiwi";
-document.getElementById("subs").innerHTML = str.substring(9,16);
+document.getElementById("subs").innerHTML = str.substring(9, 16);
 console.log(str);
+
+//Método replace()
+
+function reemplaza() {
+  let textoReemplazable = document.getElementById("replace").innerHTML;
+  document.getElementById("replace").innerHTML = textoReemplazable.replace("Microsoft", "W3Schools");
+}
