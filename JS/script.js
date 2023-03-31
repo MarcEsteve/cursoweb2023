fa2862752c8b020c7a0ff97daa067870ef147c1a
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-03-31 18:31:45 +0200

    metodo prompt() ejempo

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,11 @@ if (persona != null) {
   document.getElementById("persona").innerHTML =
     "Hola " + persona + "! Qué tal?";
 }
+
+let color1 = prompt("Escribe un color", "Ejemplo: rojo");
+let color2 = prompt("Escribe un segundo color", "Ejemplo: amarillo");
+let color3 = prompt("Escribe un tercer color", "Ejemplo: azul");
+
+if ((color1 != null) && (color2 != null) && (color1 != null)) {
+    document.getElementById("colores").innerHTML = "Los colores son: " + color1 + ", " + color2  + " y " + color3;
+}
