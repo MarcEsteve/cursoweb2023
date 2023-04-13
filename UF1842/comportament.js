546c1ed41c2b4f5f627bc242734dd4b31b1ca5fd
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-13 16:08:53 +0200

    update 4

--- UF1842/comportament.js
+++ UF1842/comportament.js
@@ -1,9 +1,13 @@
+//Actividad 4 A
 function agranda() {
     document.getElementById("paragraf").style.fontSize="35px";
 }
 
+// Actividad 4 B
 function rojo() {
     document.getElementById("titol").style.color="red";
 }
 
 function nombre3() {}
+
+//Ejercicio 3 era crear las 3 funciones vacias inicialmente
