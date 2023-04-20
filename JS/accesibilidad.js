6981a841a94752992ff8fc372891976bc8db14fc
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-20 16:56:17 +0200

    más sobre accesibilidad tambien en JS

---
+++ JS/accesibilidad.js
@@ -0,0 +1,9 @@
+function animateButton() {
+    var button = document.getElementById("anim-button");
+    button.classList.add("animate");
+  
+    setTimeout(function() {
+      button.classList.remove("animate");
+    }, 300);
+  }
+  
