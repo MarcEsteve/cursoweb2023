632fb96cfacc0ef5ff0d324df35b1ac2506529a5
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-20 18:02:21 +0200

    update

--- JS/accesibilidad.js
+++ JS/accesibilidad.js
@@ -1,9 +1,24 @@
 function animateButton() {
-    var button = document.getElementById("anim-button");
-    button.classList.add("animate");
-  
-    setTimeout(function() {
-      button.classList.remove("animate");
-    }, 300);
+  var button = document.getElementById("anim-button");
+  button.classList.add("animate");
+
+  setTimeout(function () {
+    button.classList.remove("animate");
+  }, 300);
+}
+
+var gif = document.getElementById("blinking-gif");
+var isBlinking = false;
+
+function toggleBlinking() {
+  if (isBlinking) {
+    clearInterval(blinkingInterval);
+    gif.style.opacity = "1";
+    isBlinking = false;
+  } else {
+    var blinkingInterval = setInterval(function () {
+      gif.style.opacity = gif.style.opacity === "1" ? "0" : "1";
+    }, 50);
+    isBlinking = true;
   }
-  
+}
